680a643e9a9c13e553d72cdbc15d1a4d5be32146
Buddha Lo <lo_977@yahoo.com>
2024-02-29 22:52:26 -0800

    done

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks();
+});
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  return fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json())
-  .then(books => renderBooks(books))
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((books) => renderBooks(books));
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+function renderBooks(items) {
+  const main = document.querySelector("main");
+  items.forEach((item) => {
+    const h2 = document.createElement("h2");
+    h2.textContent = item.name;
     main.appendChild(h2);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
